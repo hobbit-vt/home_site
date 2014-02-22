dc2a62def45eb034533a507e9e96d068f1a94af9
Hobbit <tohobbit@gmail.com>
2014-02-22 15:19:41 +0400

    Create Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyBegin::Application.config.secret_key_base = '10bcb947f7e82ffbd7b768514c932699be2f4bea30fcca2b52e6d3ef7cc60c78ad2915da2e6abef6742f11b4ba151705f01bf6ab08a1ba2d96fc5e354a1e2e06'
